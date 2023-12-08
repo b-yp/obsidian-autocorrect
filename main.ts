8c5209b1bfa149118632616cf07a5f2db18d0a3d
b-yp <bypl@vip.qq.com>
2023-12-08 15:49:54 +0800

    fix: Fixed the issue of not retrieving the most recent edited file

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { MarkdownView, Plugin } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from 'obsidian';
 import init, { format } from 'autocorrect-wasm'
 import { v4 as uuidv4 } from 'uuid'
 
@@ -7,18 +7,12 @@ import autocorrectWasm from './pkg/autocorrect_wasm_bg.wasm'
 
 export default class AutocorrectPlugin extends Plugin {
 	async onload() {
-		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		if (!markdownView) {
-			return;
-		}
-
 		await init(autocorrectWasm)
 
 		this.addCommand({
 			id: 'format-content',
 			name: 'Format content',
-			callback: () => {
-				const editor = markdownView.editor;
+			editorCallback: editor => {
 				let content = editor.getValue();
 
 				const tagReg = /#[^\s#]+/g
